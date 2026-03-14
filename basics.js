f8fd0a1a6766769f980fa3cba0d411a81647ef52
DenisBode <Denis.Bode@gmx.de>
2026-03-14 10:13:50 +0100

    add index.html, basics.js and sings_numbers.js

---
+++ basics.js
@@ -0,0 +1,31 @@
+// Variablen
+
+// let (veränderbar)
+
+let myVariable = 22;
+
+console.log(myVariable);
+
+// const (nicht veränderbar)
+
+const myOtherVariable = 42;
+
+console.log(myOtherVariable);
+
+// Datentypen
+
+let myStirng = 'http://127.0.0.1:5500/index.html'; // Sting is text
+
+
+let myNumber = 52.4; // only a number
+let myFloat = 52.3265;  // float is a number with decimal place
+let myInt = 44; // Integer is a Integer (german Ganzzahl)
+
+let myBoolean = true // true or false
+
+let myArray = [2, 7, 'was zur Hölle']; // a list
+
+let myObject = { age: 32, height: 183 }; // a list with key/value pairs
+
+
+console.log(myNumber, myFloat, myInt);
---
+++ strings_numbers.js
@@ -0,0 +1,37 @@
+// basic arithmetic operations   
+
+let myDivision = 15 / 5;
+let myMulty = 5 * 5;
+let myAddition = 5 + 5;
+let mySubtraction = 88 - 44;
+
+console.log(myAddition, myDivision, myMulty, mySubtraction);
+
+
+// strings
+
+let myConcat = 'hello' + ' world' + '!'
+
+console.log(myConcat);
+
+// combination string and numbers
+
+let myFirstCombination = 5 + '5'; // -> 55
+console.log(myFirstCombination);
+
+let mySecondCombination = '5' + '5'; // -> 55
+console.log(mySecondCombination);
+
+let myThirdCombi = 5 - '3'; // -> 2
+console.log(myThirdCombi);
+
+let myFourthCombi = '5' - 3; // -> 2
+console.log(myFourthCombi);
+
+let myFithCombi = '5,0' - 3.1; // -> NaN -> NotaNumber
+console.log(myFithCombi);
+
+let mySixtCombi = '5.5' - 3.1; // -> 2,4
+console.log(mySixtCombi);
+
+
