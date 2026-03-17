3f25c6d425b30350add7e06657c20d79762f28c6
DenisBode <Denis.Bode@gmx.de>
2026-03-17 09:19:29 +0100

    add comments to difference tests

--- uebung.js
+++ uebung.js
@@ -1,10 +1,18 @@
+// test 1
+
 let fullName = (firstname, lastname) => {
     return firstname + " " + lastname;
 };
 
 console.log(fullName("Max", "Mustermann"));
 
+
+//test 2
+
 function capitalizeLetters(text) {
     return text.toUpperCase();
 }
 console.log(capitalizeLetters("huch das wird ja groß zurückgegeben"));
+
+//test 3
+
